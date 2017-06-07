b416ffd4717d55d3933b1703047ac163008525c6
Andrew Beekhof <andrew@beekhof.net>
2017-06-07 22:43:19 +1000

    Split search functionality off into a separate file

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 import (
     "fmt"
-    "io/ioutil"
     "os"
-    "encoding/json"
     "strings"
     "flag"
     "bufio"
-    "strconv"
-    "reflect"
 )
-
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
-
-func Filter(vs []map[string]interface{}, f func(map[string]interface{}) bool) []map[string]interface{} {
-    vsf := make([]map[string]interface{}, 0)
-    for _, v := range vs {
-        if f(v) {
-            vsf = append(vsf, v)
-        }
-    }
-    return vsf
-}
 
 func main() {
 
@@ -35,7 +15,7 @@ func main() {
 
     flag.Parse()
     if len(*filename) > 0 && len(*search_key) > 0 {
-        if search(*filename, *search_key, *search_value) > 0 {
+        if search_file(*filename, *search_key, *search_value) > 0 {
            os.Exit(0)
         }
         os.Exit(1) // No result found
@@ -56,7 +36,7 @@ func main() {
                os.Exit(0)
            case "1":
               filename, key, value := request_search_fields(scanner)
-              search(filename, key, value)
+              search_file(filename, key, value)
            case "2":
               println("Not implemented yet")
            default:
@@ -118,64 +98,3 @@ func request_search_fields(scanner *bufio.Scanner) (string, string, string) {
     return filename, search_key, search_value
 }
 
-func search(filename string, search_key string, search_value string) int {
-    // TODO: Test with really big data sets to see if its worth caching the file contents.
-    //       Assumes repeated searches of the same dataset in interactive mode 
-
-    println("Reading", filename)
-    bytes, err := ioutil.ReadFile(filename)
-    check(err)
-
-    println("Parsing", filename)
-
-    // We expect an array, of maps, of strings to $something
-    var jdat []map[string]interface{}
-
-    // TODO: Test behaviour of parser with malformed and "valid json,
-    // but not arranged like this" input
-    if err := json.Unmarshal(bytes, &jdat); err != nil {
-        panic(err)
-    }
-
-    println("Filtering", len(jdat), "records based on", search_key, "=", search_value)
-    results := Filter(jdat, func(v map[string]interface{}) bool {
-        value := v[search_key]
-        if value == nil && len(search_value) == 0 {
-            // Searching for empty values
-            return true
-        } else if value == nil {
-            return false
-        }
-        string_value := ""
-
-        switch value.(type) {
-        case bool:
-            string_value = strconv.FormatBool(value.(bool));
-        case int64:
-            string_value = strconv.FormatInt(value.(int64), 10);
-        case float64:
-            string_value = strconv.FormatFloat(value.(float64), 'f', 0, 64);
-        case string:
-            string_value = value.(string)
-	case []interface {}:
-	    // TODO: Handle arrays
-	    return false
-        default:
-            unhandled := reflect.Indirect(reflect.ValueOf(value))
-            fmt.Println("Unhandled type", unhandled.Kind(), "in filter for key", search_key)
-            return false
-        }
-
-        if strings.Compare(string_value, search_value) == 0 {
-            return true
-        }
-        return false
-    })
-    num_results := len(results)
-    if num_results > 0 {
-        enc := json.NewEncoder(os.Stdout)
-        enc.Encode(results)
-    }
-    println(num_results, "record(s) found")
-    return num_results
-}
---
+++ search.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+    "encoding/json"
+    "strings"
+    "strconv"
+    "reflect"
+)
+
+func search_file(filename string, search_key string, search_value string) int {
+    // TODO: Test with really big data sets to see if its worth caching the file contents.
+    //       Assumes repeated searches of the same dataset in interactive mode 
+
+    println("Reading", filename)
+    bytes, err := ioutil.ReadFile(filename)
+    check(err)
+
+    println("Parsing", filename)
+
+    // We expect an array, of maps, of strings to $something
+    var json_data []map[string]interface{}
+
+    // TODO: Test behaviour of parser with malformed and "valid json,
+    // but not arranged like this" input
+    if err := json.Unmarshal(bytes, &json_data); err != nil {
+        panic(err)
+    }
+    return search_json(json_data, search_key, search_value)
+}
+
+func search_json(json_data []map[string]interface{}, search_key string, search_value string) int {
+
+    println("Filtering", len(json_data), "records based on", search_key, "=", search_value)
+    results := Filter(json_data, func(v map[string]interface{}) bool {
+        value := v[search_key]
+        if value == nil && len(search_value) == 0 {
+            // Searching for empty values
+            return true
+        } else if value == nil {
+            return false
+        }
+        string_value := ""
+
+        switch value.(type) {
+        case bool:
+            string_value = strconv.FormatBool(value.(bool));
+        case int64:
+            string_value = strconv.FormatInt(value.(int64), 10);
+        case float64:
+            string_value = strconv.FormatFloat(value.(float64), 'f', 0, 64);
+        case string:
+            string_value = value.(string)
+	case []interface {}:
+	    // TODO: Handle arrays
+	    return false
+        default:
+            unhandled := reflect.Indirect(reflect.ValueOf(value))
+            fmt.Println("Unhandled type", unhandled.Kind(), "in filter for key", search_key)
+            return false
+        }
+
+        if strings.Compare(string_value, search_value) == 0 {
+            return true
+        }
+        return false
+    })
+    num_results := len(results)
+    if num_results > 0 {
+        enc := json.NewEncoder(os.Stdout)
+        enc.Encode(results)
+    }
+    println(num_results, "record(s) found")
+    return num_results
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func Filter(vs []map[string]interface{}, f func(map[string]interface{}) bool) []map[string]interface{} {
+    vsf := make([]map[string]interface{}, 0)
+    for _, v := range vs {
+        if f(v) {
+            vsf = append(vsf, v)
+        }
+    }
+    return vsf
+}
