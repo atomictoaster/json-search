296a5907bc9e9d7a8824348cded6ea966ed8da14
Andrew Beekhof <andrew@beekhof.net>
2017-06-07 20:32:02 +1000

    Basic search and filtering

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "io/ioutil"
     "os"
     "encoding/json"
+    "strings"
 )
 
 func check(e error) {
@@ -12,22 +13,57 @@ func check(e error) {
     }
 }
 
+func Any(vs []map[string]interface{}, f func(map[string]interface{}) bool) bool {
+    for _, v := range vs {
+        if f(v) {
+            return true
+        }
+    }
+    return false
+}
+
+func Filter(vs []map[string]interface{}, f func(map[string]interface{}) bool) []map[string]interface{} {
+    vsf := make([]map[string]interface{}, 0)
+    for _, v := range vs {
+        if f(v) {
+            vsf = append(vsf, v)
+        }
+    }
+    return vsf
+}
+
 func main() {
-    println("Opening file")
-    bytes, err := ioutil.ReadFile("./users.json")
+    filename := "./users.json"
+    search_key := "name"
+    search_value := "Catalina Simpson"
+    
+    println("Reading", filename)
+    bytes, err := ioutil.ReadFile(filename)
     check(err)
     fmt.Print(string(bytes))
 
-    println("Parsing JSON")
-    var jdat interface{}
+    println("Parsing", filename)
+
+    // We expect an array, of maps, of strings to $something
+    var jdat []map[string]interface{}
+
+    // TODO: Test behaviour of parser with malformed and "valid json
+    // but not arranged like this" input
     if err := json.Unmarshal(bytes, &jdat); err != nil {
         panic(err)
     }
 
-    //println("Native print")
-    //fmt.Println(jdat)
+    println("Searching for", search_key, "=", search_value)
+    fmt.Println(Any(jdat, func(v map[string]interface{}) bool {
+	name := v[search_key].(string)
+        return strings.HasPrefix(name, search_value)
+    }))
 
-    println("JSON print")
+    println("Filtering based on", search_key, "=", search_value)
     enc := json.NewEncoder(os.Stdout)
-    enc.Encode(jdat)
+    enc.Encode(Filter(jdat, func(v map[string]interface{}) bool {
+	name := v[search_key].(string)
+        return strings.HasPrefix(name, search_value)
+    }))
+
 }
