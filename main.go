8b12f3c8aace4c9a873da2b6205f684ef54445d9
Andrew Beekhof <andrew@beekhof.net>
2017-06-07 20:43:22 +1000

    Command line options

--- main.go
+++ main.go
@@ -5,21 +5,13 @@ import (
     "os"
     "encoding/json"
     "strings"
+    "flag"
 )
 
 func check(e error) {
     if e != nil {
         panic(e)
     }
-}
-
-func Any(vs []map[string]interface{}, f func(map[string]interface{}) bool) bool {
-    for _, v := range vs {
-        if f(v) {
-            return true
-        }
-    }
-    return false
 }
 
 func Filter(vs []map[string]interface{}, f func(map[string]interface{}) bool) []map[string]interface{} {
@@ -33,16 +25,18 @@ func Filter(vs []map[string]interface{}, f func(map[string]interface{}) bool) []
 }
 
 func main() {
-    filename := "./users.json"
-    search_key := "name"
-    search_value := "Catalina Simpson"
-    
-    println("Reading", filename)
-    bytes, err := ioutil.ReadFile(filename)
+    filename := flag.String("filename", "./users.json", "File containing valid JSON")
+    search_key := flag.String("key", "name", "JSON key to search for")
+    search_value := flag.String("value", "Catalina Simpson", "Value which the specified key must contain")
+
+    flag.Parse()
+
+    println("Reading", *filename)
+    bytes, err := ioutil.ReadFile(*filename)
     check(err)
     fmt.Print(string(bytes))
 
-    println("Parsing", filename)
+    println("Parsing", *filename)
 
     // We expect an array, of maps, of strings to $something
     var jdat []map[string]interface{}
@@ -53,17 +47,12 @@ func main() {
         panic(err)
     }
 
-    println("Searching for", search_key, "=", search_value)
-    fmt.Println(Any(jdat, func(v map[string]interface{}) bool {
-	name := v[search_key].(string)
-        return strings.HasPrefix(name, search_value)
-    }))
-
-    println("Filtering based on", search_key, "=", search_value)
+    println("Filtering based on", *search_key, "=", *search_value)
     enc := json.NewEncoder(os.Stdout)
     enc.Encode(Filter(jdat, func(v map[string]interface{}) bool {
-	name := v[search_key].(string)
-        return strings.HasPrefix(name, search_value)
+        // TODO: Handle non-string fields, eg. arrays and even numbers
+	name := v[*search_key].(string)
+        return strings.HasPrefix(name, *search_value)
     }))
 
 }
